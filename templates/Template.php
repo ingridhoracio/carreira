ace91eaed1f397d5bad3d7bf39f653a2acea0e98
ingrid <ingrid@gmail>
2023-08-29 13:50:41 -0300

    .

---
+++ templates/Template.php
@@ -0,0 +1,14 @@
+<?php
+class Template {
+    function header(){
+        require __DIR__ . "/template/header.php";
+    }
+
+    function footer(){
+        require __DIR__ . "/template/footer.php";
+    }
+}
+
+
+
+?>
