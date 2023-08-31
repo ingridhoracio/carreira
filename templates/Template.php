d77abade0714cd3420ef62ab131263c47d4a2928
Ingrid <Ingrid@gmail>
2023-08-31 11:35:34 -0300

    aula 31_08

---
+++ templates/Template.php
@@ -0,0 +1,13 @@
+<?php
+
+class Template{
+    function header(){
+        require __DIR__ . "/header.php";
+    }
+
+    function footer(){
+        require __DIR__ . "/footer.php";
+    }
+}
+
+?>
---
+++ templates/header.php
@@ -0,0 +1,7 @@
+<header>
+    <nav>
+        <img src="static/imagens/logo.png" class="logo">
+        <div id="titulo">CarreiraEtec</div>
+    </nav>
+
+</header>
