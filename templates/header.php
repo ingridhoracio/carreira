cf5a4fe73ae4344d98e59dca20332c2a28ac7b5e
Ingrid <Ingrid@gmail.com>
2023-09-28 11:11:00 -0300

    atualizacao

--- templates/header.php
+++ templates/header.php
@@ -1,7 +1,31 @@
+<link href="static/css/header.css" rel='stylesheet'>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+
+
 <header>
-    <nav>
-        <img src="static/imagens/logo.png" class="logo">
-        <div id="titulo">CarreiraEtec</div>
+
+
+    <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="static/imagens/logo.png" alt="Logo" width="60" height="60"
+                    class="d-inline-block align-text-top">
+                    CarreiraETEC
+            </a>
+
+            <ul class="navbar-nav mb-2 mb-lg-0 ">
+                <li class="nav-item">
+                    <a class="nav-link" href="empresa">Empresas</a>
+                </li>
+            </ul>
+        </div>
     </nav>
+
 
 </header>
