effed8a2bb3a1e12e16f3fe84dcef81f1ef8c11b
Ingrid <Ingrid@gmail.com>
2023-09-28 11:30:42 -0300

    aula 28_09

--- Router.php
+++ Router.php
@@ -4,20 +4,27 @@ require_once __DIR__ . '/RouteSwitch.php';
 
 class Router extends RouterSwitch
 {
-    /**
-     * Summary of run
-     * @param string $requestUri
-     * @return void
-     */
+
     public function run(string $requestUri)
     {
         $route = substr($requestUri, 1);
-        $route = str_replace("carreira/", "", $route);
+        $route = str_replace("carreira/", "",0);
 
-        if ($route == '') {
+        error_log("Rota requisitada: $route",0);
+        $parts = explode("/",$route); //quebrando a requisição
+        error_log("Partes da requisição: ".print_r($parts,true),0);
+
+        $page = $parts[0]; //obtenho a pagina
+        $operation = count($parts)>1?$parts[1]:null; //obtenho a operacao se for solicitada
+
+
+        if ($route =='') {
             $this->home();
         } else {
-            $this->$route();
+            $this->$page($operation);
         }
     }
 }
+        
+    
+   
